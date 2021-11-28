98ed4edc09aede10fc3de279d8ab77ab92bb3fbc
Pierre Chevallier <pierre.chevallier1@etu.univ-grenoble-alpes.fr>
2021-11-28 12:24:21 +0100

    Add pokemonBorderCard directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BorderCardDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
